8b8c39c604fd1482df040fa299005ea1f4231bfa
Arash Iranzadeh <arash.iranzadeh1980@gmail.com>
2025-09-02 21:48:38 +0200

    Update qc_cleaning_detection.sh

--- scripts/qc_cleaning_detection.sh
+++ scripts/qc_cleaning_detection.sh
@@ -13,10 +13,13 @@ set -euo pipefail
 module load fastqc multiqc trimmomatic fastp kraken2 
 THREADS=$(nproc)
 # Input locations
-TB_RAW="/cbio/training/courses/2025/micmet-genomics/Dataset_Mt_Vc/tb/raw_data"
-VC_RAW="/cbio/training/courses/2025/micmet-genomics/Dataset_Mt_Vc/vc/raw_data"
+TB_RAW="/data/Dataset_Mt_Vc/tb/raw_data"
+VC_RAW="/data/Dataset_Mt_Vc/vc/raw_data"
 # Trimmomatic adapters
-ADAPT_COMBO="/cbio/training/courses/2025/micmet-genomics/timmomatic_adapter_Combo.fa"
+ADAPT_COMBO="/data/timmomatic_adapter_Combo.fa"
+# saving sample IDs
+ls ${TB_RAW} | cut -f1 -d '_'  | sort | uniq > tb_IDs
+ls ${VC_RAW} | cut -f1 -d '_'  | sort | uniq > vc_IDs
 
 # Output folders
 mkdir -p results/qc_raw/tb results/qc_raw/vc
@@ -29,14 +32,17 @@ mkdir -p results/kraken2_trimmomatic/tb results/kraken2_trimmomatic/vc
 # Quality check
 # For long read data use longqc:
 https://github.com/yfukasawa/LongQC
-time1=$SECONDS
-echo "=== 1) FastQC on RAW reads + MultiQC ==="
+# 1) FastQC on RAW reads + MultiQC ==="
+
 # TB
 fastqc -t ${THREADS} -o results/qc_raw/tb ${TB_RAW}/*_1.fastq.gz ${TB_RAW}/*_2.fastq.gz
 multiqc results/qc_raw/tb -n tb_multiqc_raw.html -o results/qc_raw/tb
-# VC
+#
+VC
 fastqc -t ${THREADS} -o results/qc_raw/vc ${VC_RAW}/*_1.fastq.gz ${VC_RAW}/*_2.fastq.gz
 multiqc results/qc_raw/vc -n vc_multiqc_raw.html -o results/qc_raw/vc
+
+
 
 # Data cleaning
 # for long read data use:
@@ -46,44 +52,47 @@ multiqc results/qc_raw/vc -n vc_multiqc_raw.html -o results/qc_raw/vc
 # MinIONQC and NanoPack: A set of tools for quality control and filtering of Nanopore data.
 # SMRT Link: PacBio's official software for primary analysis (demultiplexing, adapter removal, Consensus Sequences (generation).
 
-echo "=== 2) Trimming with TRIMMOMATIC (saved in results/trimmed_trimmomatic) ==="
+
+# 2) Cleaning with TRIMMOMATIC 
 # TB (MINLEN 50)
-for R1 in ${TB_RAW}/*_1.fastq.gz; do
-  [ -e "$R1" ] || continue
-  R2=${R1/_1.fastq.gz/_2.fastq.gz}
-  SAMPLE=$(basename "$R1" "_1.fastq.gz")
+for SAMPLE in $(cat tb_IDs); do
   echo "[TB|Trimmomatic] $SAMPLE"
   trimmomatic PE -threads ${THREADS} -phred33 \
-    "$R1" "$R2" \
-    "results/trimmed_trimmomatic/tb/${SAMPLE}_1P.fastq.gz" "results/trimmed_trimmomatic/tb/${SAMPLE}_1U.fastq.gz" \
-    "results/trimmed_trimmomatic/tb/${SAMPLE}_2P.fastq.gz" "results/trimmed_trimmomatic/tb/${SAMPLE}_2U.fastq.gz" \
+    ${TB_RAW}/${SAMPLE}_1.fastq.gz ${TB_RAW}/${SAMPLE}_2.fastq.gz  \
+    "results/trimmed_trimmomatic/tb/${SAMPLE}_1.fastq.gz" "results/trimmed_trimmomatic/tb/${SAMPLE}_1U.fastq.gz" \
+    "results/trimmed_trimmomatic/tb/${SAMPLE}_2.fastq.gz" "results/trimmed_trimmomatic/tb/${SAMPLE}_2U.fastq.gz" \
     ILLUMINACLIP:"${ADAPT_COMBO}":2:30:10 SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:50
 done
 
 # VC (MINLEN 50)
-for R1 in ${VC_RAW}/*_1.fastq.gz; do
-  [ -e "$R1" ] || continue
-  R2=${R1/_1.fastq.gz/_2.fastq.gz}
-  SAMPLE=$(basename "$R1" "_1.fastq.gz")
-  echo "[VC|Trimmomatic] $SAMPLE"
+for SAMPLE in $(cat vc_IDs); do
   trimmomatic PE -threads ${THREADS} -phred33 \
-    "$R1" "$R2" \
-    "results/trimmed_trimmomatic/vc/${SAMPLE}_1P.fastq.gz" "results/trimmed_trimmomatic/vc/${SAMPLE}_1U.fastq.gz" \
-    "results/trimmed_trimmomatic/vc/${SAMPLE}_2P.fastq.gz" "results/trimmed_trimmomatic/vc/${SAMPLE}_2U.fastq.gz" \
+    ${VC_RAW}/${SAMPLE}_1.fastq.gz ${VC_RAW}/${SAMPLE}_2.fastq.gz  \
+    "results/trimmed_trimmomatic/vc/${SAMPLE}_1.fastq.gz" "results/trimmed_trimmomatic/vc/${SAMPLE}_1U.fastq.gz" \
+    "results/trimmed_trimmomatic/vc/${SAMPLE}_2.fastq.gz" "results/trimmed_trimmomatic/vc/${SAMPLE}_2U.fastq.gz" \
     ILLUMINACLIP:"${ADAPT_COMBO}":2:30:10 SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:50
 done
-# To save soace, you can remove unpaired reads:
+
+# you can remove unpaired reads to save space:
 rm ./results/trimmed_trimmomatic/*/*U*
 
-echo "=== 3) Trimming with FASTP (saved in results/trimmed_fastp) ==="
+# Quality check after cleaning by trimmomati
+fastqc -t ${THREADS} -o results/qc_trim_trimmomatic/tb results/trimmed_trimmomatic/tb/*_1.fastq.gz results/trimmed_trimmomatic/tb/*_2.fastq.gz
+fastqc -t ${THREADS} -o results/qc_trim_trimmomatic/vc results/trimmed_trimmomatic/vc/*_1.fastq.gz results/trimmed_trimmomatic/vc/*_2.fastq.gz
+
+multiqc results/qc_trim_trimmomatic/tb -n tb_multiqc_trimmed_trimmomatic.html -o results/qc_trim_trimmomatic/tb
+multiqc results/qc_trim_trimmomatic/vc -n vc_multiqc_trimmed_trimmomatic.html -o results/qc_trim_trimmomatic/vc
+
+
+
+
+
+# Cleaning by fastp (optional)
 # TB (length_required 50) + polyG/polyX trimming
-for R1 in ${TB_RAW}/*_1.fastq.gz; do
-  [ -e "$R1" ] || continue
-  R2=${R1/_1.fastq.gz/_2.fastq.gz}
-  SAMPLE=$(basename "$R1" "_1.fastq.gz")
-  echo "[TB|fastp] $SAMPLE"
-  ${FASTP} -w ${THREADS} \
-    -i "$R1" -I "$R2" \
+for SAMPLE in $(cat tb_IDs); do
+  echo "[TB|fastp] ${SAMPLE}"
+  fastp -w ${THREADS} \
+    -i ${TB_RAW}/${SAMPLE}_1.fastq.gz  -I ${TB_RAW}/${SAMPLE}_2.fastq.gz \
     -o "results/trimmed_fastp/tb/${SAMPLE}_1.fastq.gz" \
     -O "results/trimmed_fastp/tb/${SAMPLE}_2.fastq.gz" \
     --detect_adapter_for_pe --trim_poly_g --trim_poly_x \
@@ -92,13 +101,10 @@ for R1 in ${TB_RAW}/*_1.fastq.gz; do
 done
 
 # VC (length_required 50) + polyG/polyX trimming
-for R1 in ${VC_RAW}/*_1.fastq.gz; do
-  [ -e "$R1" ] || continue
-  R2=${R1/_1.fastq.gz/_2.fastq.gz}
-  SAMPLE=$(basename "$R1" "_1.fastq.gz")
-  echo "[VC|fastp] $SAMPLE"
-  ${FASTP} -w ${THREADS} \
-    -i "$R1" -I "$R2" \
+for SAMPLE in $(cat vc_IDs); do
+  echo "[VC|fastp] ${SAMPLE}"
+  fastp -w ${THREADS} \
+    -i ${VC_RAW}/${SAMPLE}_1.fastq.gz  -I ${VC_RAW}/${SAMPLE}_2.fastq.gz \
     -o "results/trimmed_fastp/vc/${SAMPLE}_1.fastq.gz" \
     -O "results/trimmed_fastp/vc/${SAMPLE}_2.fastq.gz" \
     --detect_adapter_for_pe --trim_poly_g --trim_poly_x \
@@ -106,22 +112,15 @@ for R1 in ${VC_RAW}/*_1.fastq.gz; do
     -h "results/trimmed_fastp/vc/${SAMPLE}_fastp.html" 
 done
 
-# Quality check after trimming again
-echo "=== 4) FastQC on TRIMMED reads + MultiQC (Trimmomatic vs fastp) ==="
-# Trimmomatic QC
-fastqc -t ${THREADS} -o results/qc_trim_trimmomatic/tb results/trimmed_trimmomatic/tb/*_1.fastq.gz results/trimmed_trimmomatic/tb/*_2.fastq.gz
-fastqc -t ${THREADS} -o results/qc_trim_trimmomatic/vc results/trimmed_trimmomatic/vc/*_1.fastq.gz results/trimmed_trimmomatic/vc/*_2.fastq.gz
-multiqc results/qc_trim_trimmomatic/tb -n tb_multiqc_trimmed_trimmomatic.html -o results/qc_trim_trimmomatic/tb
-multiqc results/qc_trim_trimmomatic/vc -n vc_multiqc_trimmed_trimmomatic.html -o results/qc_trim_trimmomatic/vc
-
-# fastp QC
+# Quality check after cleaning by fastp
 fastqc -t ${THREADS} -o results/qc_trim_fastp/tb results/trimmed_fastp/tb/*_1.fastq.gz results/trimmed_fastp/tb/*_2.fastq.gz
 fastqc -t ${THREADS} -o results/qc_trim_fastp/vc results/trimmed_fastp/vc/*_1.fastq.gz results/trimmed_fastp/vc/*_2.fastq.gz
+
 multiqc results/qc_trim_fastp/tb -n tb_multiqc_trimmed_fastp.html -o results/qc_trim_fastp/tb
 multiqc results/qc_trim_fastp/vc -n vc_multiqc_trimmed_fastp.html -o results/qc_trim_fastp/vc
 
-time2=$SECONDS
-echo "Elapsed time for QC and cleaning of all samples: $(((time2 - time1)/60)) minutes!"
+
+
 
 # ===========================================
 # DB BUILDING + DETECTION
@@ -136,39 +135,35 @@ echo "Elapsed time for QC and cleaning of all samples: $(((time2 - time1)/60)) m
 # kraken2-build --build --threads ${THREADS} --db ./kraken2_db_bacteria
 
 KRAKEN_DB="/cbio/training/courses/2025/micmet-genomics/kraken2_db_standard"
-time1=$SECONDS
-echo "=== 5) Kraken2 on FASTP-cleaned reads (if DB available) ==="
+# 5) Kraken2 on FASTP-cleaned reads (if DB available) ==="
 # TB
-for R1P in results/trimmed_trimmomatic/tb/*_1P.fastq.gz; do
-  R2P=${R1P/_1P.fastq.gz/_2P.fastq.gz}
-  SAMPLE=$(basename "$R1P" "_1_1P.fastq.gz")
-  echo "[TB|Kraken2] $SAMPLE"
-  kraken2 --db "$KRAKEN_DB" --threads ${THREADS} \
+for SAMPLE in $(cat tb_IDs); do
+ kraken2 --db "$KRAKEN_DB" --threads ${THREADS} \
     --quick --confidence 0.1 --memory-mapping --gzip-compressed --use-names \
-    --paired "$R1P" "$R2P" \
+    --paired ./results/trimmed_trimmomatic/tb/${SAMPLE}_1.fastq.gz ./results/trimmed_trimmomatic/tb/${SAMPLE}_2.fastq.gz \
     --report "results/kraken2_trimmomatic/tb/${SAMPLE}.report" \
     --output "results/kraken2_trimmomatic/tb/${SAMPLE}.kraken"
 done
 
 # VC
-for R1P in results/trimmed_trimmomatic/vc/*_1P.fastq.gz; do
- R2P=${R1P/_1P.fastq.gz/_2P.fastq.gz}
- SAMPLE=$(basename "$R1P" "_1_1P.fastq.gz")
- echo "[VC|Kraken2] $SAMPLE"
+for SAMPLE in $(cat vc_IDs); do
  kraken2 --db "$KRAKEN_DB" --threads ${THREADS} \
   --quick --confidence 0.1 --memory-mapping --gzip-compressed --use-names \
-  --paired "$R1P" "$R2P" \
+  --paired /results/trimmed_trimmomatic/vc/${SAMPLE}_1.fastq.gz ./results/trimmed_trimmomatic/vc/${SAMPLE}_2.fastq.gz \
   --report "results/kraken2_trimmomatic/vc/${SAMPLE}.report" \
   --output "results/kraken2_trimmomatic/vc/${SAMPLE}.kraken"
   done
 
-time2=$SECONDS
-echo "Elapsed time for kraken2 running on all samples: $(((time2 - time1)/60)) minutes!"
+# to visualize the kraken results use Pavian that is an R/Shiny application. 
+# Install R and RStudio. Install Pavian dependencies: Open R and run the following commands.
+if (!require(remotes)) {
+    install.packages("remotes")
+}
+remotes::install_github("fbreitwieser/pavian")
 
-echo
-echo "=== DONE ==="
-echo "Open:"
-echo "  RAW MultiQC:                         results/qc_raw/tb/tb_multiqc_raw.html, results/qc_raw/vc/vc_multiqc_raw.html"
-echo "  TRIM (Trimmomatic) MultiQC:          results/qc_trim_trimmomatic/*/*.html"
-echo "  TRIM (fastp) MultiQC:                results/qc_trim_fastp/*/*.html"
-echo "  Kraken2 (Trimmomatic) reports:             results/kraken2_trimmomatic/*/*.report"
+# Run Pavian: In R type
+
+library(pavian)
+pavian::runApp(port=5000)
+
+This will start a web server and print the address in the console. Paste this address into your web browser to use the application. 
