26b340ffd54e8d36a33d825f09786d9a3ccc1877
Arash Iranzadeh <arash.iranzadeh1980@gmail.com>
2025-09-04 22:48:03 +0200

    Update de-novo_assembly_annotation.sh

--- scripts/de-novo_assembly_annotation.sh
+++ scripts/de-novo_assembly_annotation.sh
@@ -6,16 +6,13 @@ module purge
 module load spades/4.2.0
 OUTPUT_DIR="/data/users/${USER}/data_analysis"
 THREADS=$(nproc)
+
+# TB
 # Input locations
-TB_CLEAN="/data/users/user28/data_analysis/trimmed_trimmomatic/tb"
-VC_CLEAN="/data/users/user28/data_analysis/trimmed_trimmomatic/vc"
-
-# making output folders
-mkdir -p ${OUTPUT_DIR}/assembly/tb ${OUTPUT_DIR}/assembly/vc
-mkdir -p ${OUTPUT_DIR}/annotation/tb ${OUTPUT_DIR}/annotation/vc
-
+TB_CLEAN="/data/users/${USER}/data_analysis/trimmed_trimmomatic/tb"
+# Output location
+mkdir -p ${OUTPUT_DIR}/assembly/tb ${OUTPUT_DIR}/annotation/tb
 ls $TB_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/tb_IDs
-ls $VC_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/vc_IDs
 
 #TB assembly
 for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
@@ -23,14 +20,54 @@ for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
   --careful -t ${THREADS} --cov-cutoff "auto" -o ${OUTPUT_DIR}/assembly/tb/${SAMPLE}
 done
 
+#TB annotation
 module load anaconda3/2024.10
 conda activate prokka
-
 for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
   prokka --outdir ${OUTPUT_DIR}/annotation/tb/${SAMPLE} \
          --prefix ${SAMPLE} \
          --cpus ${THREADS} --genus Mycobacterium --species tuberculosis \
          --kingdom Bacteria ${OUTPUT_DIR}/assembly/tb/${SAMPLE}/contigs.fasta
 done
-
 conda deactivate
+
+# Peek at what’s annotated, count CDS, rRNA, tRNA per sample (from GFF)
+# # check the results and axplain it
+BASE=/data/users/${USER}/data_analysis/annotation/tb
+SAMPLES="ERR10225374 ERR10225402"   # start with two; you can also do: SAMPLES=$(basename -a $BASE/ERR*)
+for S in $SAMPLES; do
+  GFF=$BASE/$S/${S}.gff
+  cds=$(grep -P "\tCDS\t" "$GFF" | wc -l)
+  trna=$(grep -P "\ttRNA\t" "$GFF" | wc -l)
+  rrna=$(grep -P "\trRNA\t" "$GFF" | wc -l)
+  echo -e "${S}\tCDS:${cds}\ttRNA:${trna}\trRNA:${rrna}"
+done
+
+
+
+
+
+
+# VC 
+VC_CLEAN="/data/users/${USER}/data_analysis/trimmed_trimmomatic/vc"
+mkdir -p ${OUTPUT_DIR}/assembly/vc ${OUTPUT_DIR}/annotation/vc
+ls $VC_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/vc_IDs
+
+#VC assembly
+for SAMPLE in $(cat $OUTPUT_DIR/vc_IDs); do
+  spades.py -1 ${VC_CLEAN}/${SAMPLE}_1.fastq.gz -2 ${VC_CLEAN}/${SAMPLE}_2.fastq.gz \
+  --careful -t ${THREADS} --cov-cutoff "auto" -o ${OUTPUT_DIR}/assembly/vc/${SAMPLE}
+done
+
+#VC annotation
+module load anaconda3/2024.10
+conda activate prokka
+for SAMPLE in $(cat $OUTPUT_DIR/vc_IDs); do
+  prokka --outdir ${OUTPUT_DIR}/annotation/vc/${SAMPLE} \
+         --prefix ${SAMPLE} \
+         --cpus ${THREADS} --genus Mycobacterium --species tuberculosis \
+         --kingdom Bacteria ${OUTPUT_DIR}/assembly/vc/${SAMPLE}/contigs.fasta
+done
+conda deactivate
+
+
