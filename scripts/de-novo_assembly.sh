510852b8834c5a4a29b679cd6800c85ed83f992b
Arash Iranzadeh <arash.iranzadeh1980@gmail.com>
2025-09-04 17:43:36 +0200

    de_novo_assembly_prokka.sh

--- scripts/de-novo_assembly.sh
+++ scripts/de-novo_assembly.sh
@@ -1,2 +1,33 @@
-https://github.com/ablab/spades
+module purge
+module load spades/4.2.0
+OUTPUT_DIR="/data/users/${USER}/data_analysis"
+THREADS=$(nproc)
+# Input locations
+TB_CLEAN="/data/users/user28/data_analysis/trimmed_trimmomatic/tb"
+VC_CLEAN="/data/users/user28/data_analysis/trimmed_trimmomatic/vc"
 
+# making output folders
+mkdir -p ${OUTPUT_DIR}/assembly/tb ${OUTPUT_DIR}/assembly/vc
+mkdir -p ${OUTPUT_DIR}/annotation/tb ${OUTPUT_DIR}/annotation/vc
+
+ls $TB_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/tb_IDs
+ls $VC_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/vc_IDs
+
+#TB assembly
+for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
+  spades.py -1 ${TB_CLEAN}/${SAMPLE}_1.fastq.gz -2 ${TB_CLEAN}/${SAMPLE}_2.fastq.gz \
+  --careful -t ${THREADS} --cov-cutoff "auto" -o ${OUTPUT_DIR}/assembly/tb/${SAMPLE}
+done
+
+
+module load anaconda3/2024.10
+conda activate prokka
+
+for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
+  prokka --outdir ${OUTPUT_DIR}/annotation/tb/${SAMPLE} \
+         --prefix ${SAMPLE} \
+         --cpus ${THREADS} --genus Mycobacterium --species tuberculosis \
+         --kingdom Bacteria ${OUTPUT_DIR}/assembly/tb/${SAMPLE}/contigs.fasta
+done
+
+conda deactivate
