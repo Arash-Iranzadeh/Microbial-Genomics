784833f7b709275c4134c1ca01c441a444af1161
Arash Iranzadeh <arash.iranzadeh1980@gmail.com>
2025-09-05 16:11:37 +0200

    Update de-novo_assembly_annotation.sh

--- scripts/de-novo_assembly_annotation.sh
+++ scripts/de-novo_assembly_annotation.sh
@@ -1,70 +1,61 @@
+#!/bin/bash
+#SBATCH --job-name="assembly_annotation"
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128GB
+#SBATCH --output=./logs/assembly_annotation_out.log
+#SBATCH --error=./logs/assembly_annotation_err.log
+#SBATCH --time=24:00:00
 set -euo pipefail
 
-srun --cpus-per-task=8 --mem=32GB --time 3:00:00 --pty bash
-
 module purge
-module load spades/4.2.0 bedtools anaconda3/2024.10
+module load spades/4.2.0
 OUTPUT_DIR="/data/users/${USER}/data_analysis"
 THREADS=$(nproc)
-
-# TB
 # Input locations
-TB_CLEAN="/data/users/${USER}/data_analysis/trimmed_trimmomatic/tb"
-# Output location
-mkdir -p ${OUTPUT_DIR}/assembly/tb ${OUTPUT_DIR}/annotation/tb
-ls $TB_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/tb_IDs
+TB_CLEAN="/data/users/user28/data_analysis/trimmed_trimmomatic/tb"
+VC_CLEAN="/data/users/user28/data_analysis/trimmed_trimmomatic/vc"
 
-#TB assembly
+# making output folders
+mkdir -p ${OUTPUT_DIR}/assembly/tb ${OUTPUT_DIR}/assembly/vc
+mkdir -p ${OUTPUT_DIR}/annotation/tb ${OUTPUT_DIR}/annotation/vc
+
+ls $TB_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/tb_IDs
+ls $VC_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/vc_IDs
+
+echo "1) Genome assembly / TB ..."
 for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
   spades.py -1 ${TB_CLEAN}/${SAMPLE}_1.fastq.gz -2 ${TB_CLEAN}/${SAMPLE}_2.fastq.gz \
   --careful -t ${THREADS} --cov-cutoff "auto" -o ${OUTPUT_DIR}/assembly/tb/${SAMPLE}
 done
 
-#TB annotation
+module load anaconda3/2024.10
 conda activate prokka
+echo "2) Genome annotation / TB ..."
 for SAMPLE in $(cat $OUTPUT_DIR/tb_IDs); do
   prokka --outdir ${OUTPUT_DIR}/annotation/tb/${SAMPLE} \
          --prefix ${SAMPLE} \
          --cpus ${THREADS} --genus Mycobacterium --species tuberculosis \
          --kingdom Bacteria ${OUTPUT_DIR}/assembly/tb/${SAMPLE}/contigs.fasta
 done
+
 conda deactivate
 
-# Peek at what’s annotated, count CDS, rRNA, tRNA per sample (from GFF)
-# # check the results and axplain it
-BASE=/data/users/${USER}/data_analysis/annotation/tb
-SAMPLES="ERR10225374 ERR10225402"   # start with two; you can also do: SAMPLES=$(basename -a $BASE/ERR*)
-for S in $SAMPLES; do
-  GFF=$BASE/$S/${S}.gff
-  cds=$(grep -P "\tCDS\t" "$GFF" | wc -l)
-  trna=$(grep -P "\ttRNA\t" "$GFF" | wc -l)
-  rrna=$(grep -P "\trRNA\t" "$GFF" | wc -l)
-  echo -e "${S}\tCDS:${cds}\ttRNA:${trna}\trRNA:${rrna}"
-done
-
-# Excercise: Count the total number of features in one of your GFF files
-
-
-# VC 
-VC_CLEAN="/data/users/${USER}/data_analysis/trimmed_trimmomatic/vc"
-mkdir -p ${OUTPUT_DIR}/assembly/vc ${OUTPUT_DIR}/annotation/vc
-ls $VC_CLEAN | cut -f1 -d '_' | sort | uniq > $OUTPUT_DIR/vc_IDs
-
-#VC assembly
+# VC
+echo "1) Genome assembly / VC ..."
 for SAMPLE in $(cat $OUTPUT_DIR/vc_IDs); do
   spades.py -1 ${VC_CLEAN}/${SAMPLE}_1.fastq.gz -2 ${VC_CLEAN}/${SAMPLE}_2.fastq.gz \
   --careful -t ${THREADS} --cov-cutoff "auto" -o ${OUTPUT_DIR}/assembly/vc/${SAMPLE}
 done
 
-#VC annotation
 module load anaconda3/2024.10
 conda activate prokka
+echo "2) Genome annotation / VC ..."
 for SAMPLE in $(cat $OUTPUT_DIR/vc_IDs); do
   prokka --outdir ${OUTPUT_DIR}/annotation/vc/${SAMPLE} \
          --prefix ${SAMPLE} \
          --cpus ${THREADS} --genus Mycobacterium --species tuberculosis \
          --kingdom Bacteria ${OUTPUT_DIR}/assembly/vc/${SAMPLE}/contigs.fasta
 done
-conda deactivate
 
+conda deactivate
 
