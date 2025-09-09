11f6c100f6e27fc1e9d27eb4514914f2513a76a0
Arash Iranzadeh <arash.iranzadeh1980@gmail.com>
2025-09-09 17:29:12 +0200

    Update 4_pangenomics.sh

--- scripts/4_pangenomics.sh
+++ scripts/4_pangenomics.sh
@@ -22,8 +22,8 @@ panaroo-qc -t $(nproc) --graph_type all -i ${INPUT_DIR}/*.gff \
  --ref_db refseq.genomes.k21s1000.msh -o ${OUTPUT_DIR}/QC
 # running panaroo and creating a pangenome
 
-panaroo -i ${INPUT_DIR}/*.gff -o ${OUTPUT_DIR} --clean-mode st
-rict -a core -a pan --core_threshold 1 --quiet -t $(nproc)
+panaroo -i ${INPUT_DIR}/*.gff -o ${OUTPUT_DIR} \
+--clean-mode strict -a core -a pan --core_threshold 1 --quiet -t $(nproc)
 
 # to understand outputs: https://gthlab.au/panaroo/#/gettingstarted/output
 
@@ -32,7 +32,22 @@ cd $OUTPUT_DIR
 # filtering gene presence/absence file
 panaroo-filter-pa -i ./gene_presence_absence.csv -o . --type pseudo,length
 # phylogenetic tree construction
-iqtree2 -s core_gene_alignment.aln -pre core_tree -nt 8 -fast -m GTR
+mkdir -p ${OUTPUT_DIR}/phylogenetics; cd ${OUTPUT_DIR}/phylogenetics
+iqtree2 -s ${OUTPUT_DIR}/core_gene_alignment.aln --prefix vc_core_tree -m GTR -T AUTO
+#find all the paths that run through one particular gene in the graph up to 10 genes extension.
+cd $OUTPUT_DIR
+panaroo-gene-neighbourhood --gene trkI --graph final_graph.gml --expand_no 10 --out neighbourhood.txt
+
+
+# on ilifu
+# module load  anaconda3/2024.10
+# conda create -n pyseer -c bioconda -c conda-forge pyseer
+# conda activate pyseer
+# inside pyseer env: conda install fsm-lite; conda install seer; conda install unitig-counter unitig-caller
+
+#GWAS
+cd ${OUTPUT_DIR}; mkdir -p GWAS; cd GWAS
+
 
 
 
