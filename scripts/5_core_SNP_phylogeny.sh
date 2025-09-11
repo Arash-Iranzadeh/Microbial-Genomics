c48e9439906843a47fe480f6636e7d2be6fff7d5
Arash Iranzadeh <arash.iranzadeh1980@gmail.com>
2025-09-11 16:36:07 +0200

    Update 5_core_SNP_phylogeny.sh

--- scripts/5_core_SNP_phylogeny.sh
+++ scripts/5_core_SNP_phylogeny.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-INPUT_DIR="/data/users/${USER}/data_analysis"
+
 OUTPUT_DIR="/data/users/${USER}/data_analysis"
+
+cp -r /data/users/user28/data_analysis/trimmed_trimmomatic/tb ${OUTPUT_DIR}
+cp /data/users/user28/data_analysis/TB_H37Rv.fasta ${OUTPUT_DIR}
+cp /data/users/user28/data_analysis/Mtb_NC_000962.3_mask.bed ${OUTPUT_DIR}
+cp /data/users/user28/data_analysis/tb_problematic ${OUTPUT_DIR}
 # TB REF: https://www.ncbi.nlm.nih.gov/nuccore/NC_000962.3?report=fasta
-REF="${INPUT_DIR}/TB_H37Rv.fasta"
+REF="${OUTPUT_DIR}/TB_H37Rv.fasta"
 THREADS=$(nproc)
 
 module load anaconda3/2024.10
@@ -10,20 +15,36 @@ conda activate snippy
 #https://github.com/tseemann/snippy
 mkdir -p ${OUTPUT_DIR}/core_snp_phylogeny
 cd ${OUTPUT_DIR}/core_snp_phylogeny
-snippy-multi ${INPUT_DIR}/tb_core_snp_input_tab --ref ${REF} \
+
+for id in $(ls $OUTPUT_DIR/trimmed_trimmomatic/tb/ | cut -f 1 -d '_' | sort | uniq); do 
+forward=$(ls "$OUTPUT_DIR/trimmed_trimmomatic/tb/${id}_1.fastq.gz") 
+reverse=$(ls "$OUTPUT_DIR/trimmed_trimmomatic/tb/${id}_2.fastq.gz") 
+echo -e "${id}\t${forward}\t${reverse}"; done \
+ | grep -v -f $OUTPUT_DIR/tb_problematic > tb_core_snp_input_tab
+
+snippy-multi tb_core_snp_input_tab --ref ${REF} \
 --cpus ${THREADS}  > tb_core_snp_running.sh
 
-# bedfile to mask repetitivegenes cause false positives, or masking phage regions
-#download from here: 
-#https://github.com/tseemann/snippy/blob/master/etc/Mtb_NC_000962.3_mask.bed
+# bedfile to mask repetitive genes cause false positives, or masking phage regions
+# download from here: 
+# https://github.com/tseemann/snippy/blob/master/etc/Mtb_NC_000962.3_mask.bed
 # open tb_core_snp_running.sh and after "snp-core" in the last line:
-# add this "--mask ${INPUT_DIR}/Mtb_NC_000962.3_mask.bed"
+# add this "--mask ${OUTPUT_DIR}/Mtb_NC_000962.3_mask.bed"
 
 bash tb_core_snp_running.sh
 
-#remove all the "weird" characters and replace them with N, useful for tree-building or recombination-removal tool
+#check the number of variants
+module load bcftools
+bcftools stats core.vcf | grep ^SN
+wc -l core.tab
 
-snippy-clean_full_aln core.full.aln > clean.full.aln
+# on your desktop: 
+pip install alv
+alv core.aln
+# or use it: https://alignmentviewer.org/
+
+#remove all the "weird" characters and replace them with N, useful for tree-building or recombination-removal tool
+time snippy-clean_full_aln core.full.aln > clean.full.aln
 
 conda activate gubbins
 run_gubbins.py -p gubbins -c ${THREADS} clean.full.aln
